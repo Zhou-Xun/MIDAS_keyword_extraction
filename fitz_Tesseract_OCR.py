71d6a9f06f5b961114582634c96e1a73b56ba5aa
xunzhou <zhouzackkyxun@gmail.com>
2022-12-15 00:20:12 -0500

    initial commit

---
+++ fitz_Tesseract_OCR.py
@@ -0,0 +1,98 @@
+import fitz
+import os
+
+import sys, getopt
+from io import BytesIO
+
+
+def read_filename_and_path(pdf_directory):
+    filenames = []
+    file_path = []
+
+    # iterate over files in
+    # that directory
+    for filename in os.listdir(pdf_directory):
+        f = os.path.join(pdf_directory, filename)
+        # checking if it is a file
+        if os.path.isfile(f):
+            filenames.append(filename)
+            file_path.append(f)
+
+    return filenames, file_path
+
+
+def partial_ocr_for_one_pdf(filenames, file_path, output_dir):
+    '''
+    output: the partial ocr result for each pdf
+    :return:
+    '''
+    for filename in os.listdir(output_dir):
+        f = os.path.join(output_dir, filename)
+        os.remove(f)
+
+    for i in range(len(file_path)):
+        file = file_path[i]
+        name = filenames[i]
+        if file[-3:] == "pdf":
+            with open(file, "rb") as fh:
+                buf = BytesIO(fh.read())
+            doc = fitz.open(file, buf)
+            text = ""
+            for page in doc:
+                partial_tp = page.get_textpage_ocr(flags=0, full=False)
+                tmp = page.get_text(textpage=partial_tp).replace("\n", " ")
+                text += tmp + " "
+            with open(os.path.join(output_dir, filenames[i][:-3] + "txt"), 'w') as f:
+                f.write(text)
+
+
+def partial_ocr_for_one_page(filenames, file_path, output_dir):
+    '''
+    output: the partial ocr result for each page
+    :return: 
+    '''
+    for filename in os.listdir(output_dir):
+        f = os.path.join(output_dir, filename)
+        os.remove(f)
+
+    for i in range(len(file_path)):
+        file = file_path[i]
+        if file[-3:] == "pdf":
+            with open(file, "rb") as fh:
+                buf = BytesIO(fh.read())
+            doc = fitz.open(file, buf)
+
+            for j in range(len(doc)):
+                page = doc[j]
+                partial_tp = page.get_textpage_ocr(flags=0, full=False)
+                text = page.get_text(textpage=partial_tp).replace("\n", " ")
+
+                with open(os.path.join(output_dir, filenames[i][:-4] + "_page_" + str(j) + ".txt"), 'w') as f:
+                    f.write(text)
+
+
+def main():
+    args = sys.argv[1:]
+    try:
+        opts, args = getopt.getopt(args, "h-m:", ["mode="])
+    except getopt.GetoptError:
+        print('test.py -i <id> -q <query>')
+        sys.exit(2)
+
+    mode = "file"
+    for opt, arg in opts:
+        if opt in ("-m", "--mode"):
+            mode = arg
+
+    input_dir = "input_pdf"
+    output_dir = "output_txt"
+    filenames, file_path = read_filename_and_path(input_dir)
+    if mode == "page":
+        partial_ocr_for_one_page(filenames, file_path, output_dir)
+    else:
+        partial_ocr_for_one_pdf(filenames, file_path, output_dir)
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ output_keyword.py
@@ -0,0 +1,114 @@
+import os
+import sys, getopt
+import json
+
+sys.path.append("./model")
+import keyphrase_extraction_kbir_inspec as kbir
+import bert_uncased_keyword_extractor as bert
+# !pip install keyphrasetransformer
+from keyphrasetransformer import KeyPhraseTransformer
+
+
+def read_filename_and_path(txt_directory):
+    filenames = []
+    file_path = []
+
+    # iterate over files in
+    # that directory
+    for filename in os.listdir(txt_directory):
+        f = os.path.join(txt_directory, filename)
+        # checking if it is a file
+        if os.path.isfile(f):
+            filenames.append(filename)
+            file_path.append(f)
+
+    return filenames, file_path
+
+
+def kbir_inspec(filenames, file_path):
+    model_name = "ml6team/keyphrase-extraction-kbir-inspec"
+    extractor = kbir.KeyphraseExtractionPipeline(model=model_name)
+
+    output_dict = dict()
+
+    for i in range(len(file_path)):
+        file = file_path[i]
+        filename = filenames[i]
+        with open(file, "r") as text_file:
+            text = text_file.read()
+            key_phrases = extractor(text)
+            output_dict[filename] = list(key_phrases)
+
+    return output_dict
+
+
+def bert_keyword_extraction(filenames, file_path):
+    model_name = "yanekyuk/bert-uncased-keyword-extractor"
+    extractor = bert.KeyphraseExtractionPipeline(model=model_name)
+
+    output_dict = dict()
+
+    for i in range(len(file_path)):
+        file = file_path[i]
+        filename = filenames[i]
+        with open(file, "r") as text_file:
+            text = text_file.read()
+            key_phrases = extractor(text)
+            output_dict[filename] = list(key_phrases)
+
+    return output_dict
+
+
+def snrspeaks_keyphrase_transformer(filenames, file_path):
+    kp = KeyPhraseTransformer()
+    output_dict = dict()
+
+    for i in range(len(file_path)):
+        file = file_path[i]
+        filename = filenames[i]
+        with open(file, "r") as text_file:
+            text = text_file.read()
+            key_phrases = kp.get_key_phrases(text)
+            output_dict[filename] = key_phrases
+
+    return output_dict
+
+
+def dict_to_json(output_dict, json_path):
+
+    with open(json_path, 'w', encoding="utf-8") as file_obj:
+        json.dump(output_dict, file_obj, indent=2)
+
+
+def main():
+    args = sys.argv[1:]
+    try:
+        opts, args = getopt.getopt(args, "hm:", ["model="])
+    except getopt.GetoptError:
+        print('output_keyword.py -m <model>')
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ("-m", "--model"):
+            model = arg
+
+    txt_directory = "output_txt"
+    json_path = "output_keyword"
+    filenames, file_path = read_filename_and_path(txt_directory)
+    if model == "kbir":
+        json_path += "/kbir_output.json"
+        output_dict = kbir_inspec(filenames, file_path)
+    elif model == "bert":
+        json_path += "/bert_output.json"
+        output_dict = bert_keyword_extraction(filenames, file_path)
+    elif model == "skt":
+        json_path += "/skt_output.json"
+        output_dict = snrspeaks_keyphrase_transformer(filenames, file_path)
+
+    dict_to_json(output_dict, json_path)
+
+
+if __name__ == "__main__":
+    main()
+
+
